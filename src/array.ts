31004415fa70180030370f46330730d269b23abe
Alexander Larin <doremilarin@gmail.com>
2024-11-30 14:08:59 +0300

    chore: update chunk examples

--- src/array.ts
+++ src/array.ts
@@ -2,13 +2,13 @@
  * Creates an array of elements split into groups the length of size.
  * If array can't be split evenly, the final chunk will be the remaining elements.
  *
- * * @example
+ * @example
  *
- * _.chunk(['a', 'b', 'c', 'd'], 2);
- * // => [['a', 'b'], ['c', 'd']]
+ * chunk(['a', 'b', 'c']);
+ * // => [['a'], ['b'], ['c']]
  *
- * _.chunk(['a', 'b', 'c', 'd'], 3);
- * // => [['a', 'b', 'c'], ['d']]
+ * chunk(['a', 'b', 'c', 'd'], 2);
+ * // => [['a', 'b'] ['c', 'd']]
  */
 export function chunk<T>(array: Array<T>, size: number = 1): Array<Array<T>> {
   if (size < 1) {
